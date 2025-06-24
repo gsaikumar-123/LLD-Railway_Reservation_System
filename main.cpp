444207dd0d17bcecc2dbd995d7b8370d4c0b51be
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-06-24 15:39:49 +0530

    Add Ticket class implementation for railway reservation system

--- main.cpp
+++ main.cpp
@@ -28,3 +28,28 @@ public:
     Passenger(string n, int a, string p)
         : name(n), age(a), preference(p) {}
 };
+
+class Ticket {
+public:
+    int PNR;
+    string status;
+    int berthNumber;
+    string berthType;
+    int age;
+    string passengerName;
+
+    Ticket() : PNR(0), status(""), berthNumber(-1), berthType(""), age(0), passengerName("") {}
+
+    Ticket(int pnr, const string& stat, int berthNo, const string& bType, int ag, const string& name)
+        : PNR(pnr), status(stat), berthNumber(berthNo), berthType(bType), age(ag), passengerName(name) {}
+
+    void print() {
+        cout << PNR << "\t" 
+             << (berthNumber == -1 ? "N/A" : to_string(berthNumber)) << "\t"
+             << status << "\t"
+             << berthType << "\t"
+             << passengerName << "\t"
+             << age << endl;
+    }
+};
+
